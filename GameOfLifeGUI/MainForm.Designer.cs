299be071d74646092d42282312c5af215c83fd51
Matyasmester <matyas.zoboki@gmail.com>
2024-01-25 22:09:24 +0100

    Initial commit

---
+++ GameOfLifeGUI/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GameOfLifeGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartGameButton = new System.Windows.Forms.Button();
+            this.StopGameButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenerationLabel = new System.Windows.Forms.Label();
+            this.StepIntervalBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.StepIntervalBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartGameButton
+            // 
+            this.StartGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StartGameButton.Location = new System.Drawing.Point(720, 77);
+            this.StartGameButton.Name = "StartGameButton";
+            this.StartGameButton.Size = new System.Drawing.Size(152, 69);
+            this.StartGameButton.TabIndex = 0;
+            this.StartGameButton.Text = "Start game";
+            this.StartGameButton.UseVisualStyleBackColor = true;
+            this.StartGameButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopGameButton
+            // 
+            this.StopGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StopGameButton.Location = new System.Drawing.Point(720, 206);
+            this.StopGameButton.Name = "StopGameButton";
+            this.StopGameButton.Size = new System.Drawing.Size(152, 69);
+            this.StopGameButton.TabIndex = 1;
+            this.StopGameButton.Text = "Stop game";
+            this.StopGameButton.UseVisualStyleBackColor = true;
+            this.StopGameButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(716, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Generation:";
+            // 
+            // GenerationLabel
+            // 
+            this.GenerationLabel.AutoSize = true;
+            this.GenerationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GenerationLabel.Location = new System.Drawing.Point(825, 334);
+            this.GenerationLabel.Name = "GenerationLabel";
+            this.GenerationLabel.Size = new System.Drawing.Size(20, 24);
+            this.GenerationLabel.TabIndex = 3;
+            this.GenerationLabel.Text = "0";
+            this.GenerationLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // StepIntervalBox
+            // 
+            this.StepIntervalBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StepIntervalBox.Location = new System.Drawing.Point(720, 441);
+            this.StepIntervalBox.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.StepIntervalBox.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.StepIntervalBox.Name = "StepIntervalBox";
+            this.StepIntervalBox.Size = new System.Drawing.Size(125, 29);
+            this.StepIntervalBox.TabIndex = 4;
+            this.StepIntervalBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.StepIntervalBox.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(716, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Step interval (ms)";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StepIntervalBox);
+            this.Controls.Add(this.GenerationLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopGameButton);
+            this.Controls.Add(this.StartGameButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.StepIntervalBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartGameButton;
+        private System.Windows.Forms.Button StopGameButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label GenerationLabel;
+        private System.Windows.Forms.NumericUpDown StepIntervalBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ GameOfLifeGUI/MainForm.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace GameOfLifeGUI
+{
+    public partial class MainForm : Form
+    {
+        private Graphics graphics;
+
+        private readonly Pen pen = new Pen(Color.Black, 1);
+        
+        private readonly Brush blackBrush = new SolidBrush(Color.Black);
+
+        private readonly Brush bgColorBrush;
+
+        private const int rectSide = 15;
+
+        private readonly Size rectSize = new Size(rectSide, rectSide);
+
+        private const int sideBarBeginsAt = 700;
+
+        private int generation = 0;
+
+        private char[,] Map;
+
+        private const char Alive = 'A';
+        private const char Dead = 'D';
+
+        public MainForm()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+            this.MouseClick += onMouseClick;
+
+            this.Shown += new EventHandler(this.MainForm_Shown);
+
+            Map = new char[sideBarBeginsAt, this.Height];
+
+            bgColorBrush = new SolidBrush(this.BackColor);
+
+            FillVirtualMap();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void onMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.X > sideBarBeginsAt) return;
+
+            Point location = new Point();
+
+            location.X = (int)Math.Floor(e.X / 15.0d) * rectSide;
+            location.Y = (int)Math.Floor(e.Y / 15.0d) * rectSide;
+
+            int x = location.X;
+            int y = location.Y;
+
+            char onMap = Map[x, y];
+
+            if (onMap == Dead) Revive(x, y);
+
+            else { Kill(x, y); }
+        }
+
+        private void Kill(int x, int y)
+        {
+            Map[x, y] = Dead;
+
+            Rectangle rect = new Rectangle(new Point(x, y), rectSize);
+            graphics.FillRectangle(bgColorBrush, rect);
+            graphics.DrawRectangle(pen, rect);
+        }
+
+        private void Revive(int x, int y)
+        {
+            Map[x, y] = Alive;
+
+            Rectangle rect = new Rectangle(new Point(x, y), rectSize);
+            graphics.FillRectangle(blackBrush, rect);
+        }
+
+        private void CreateStartingMap()
+        {
+            for(int x = 0; x < sideBarBeginsAt; x += 15)
+            {
+                for(int y = 0; y < this.Height; y += 15)
+                {
+                    Rectangle rect = new Rectangle(new Point(x, y), rectSize);
+
+                    graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+
+        private void FillVirtualMap()
+        {
+            for (int x = 0; x < sideBarBeginsAt; x += rectSide)
+            {
+                for (int y = 0; y < this.Height; y += rectSide)
+                {
+                    Map[x, y] = Dead;
+                }
+            }
+        }
+
+        private int GetNumberOfAliveNeighbors(int x, int y)
+        {
+            int retval = 0;
+
+            int currX = (x - rectSide) >= 0 ? (x - rectSide) : x;
+            int currY = (y - rectSide) >= 0 ? (y - rectSide) : y;
+
+            int savedY = currY;
+
+            while (currX <= (x + rectSide) && (x + rectSide) < sideBarBeginsAt)
+            {
+                while (currY <= (y + rectSide) && (y + rectSide) < this.Height)
+                {
+                    if (currX == x && currY == y)
+                    {
+                        currY += rectSide;
+                        continue;
+                    }
+
+                    if (Map[currX, currY] == Alive) retval++;
+
+                    currY += rectSide;
+                }
+
+                currY = savedY;
+                currX += rectSide;
+            }
+
+            return retval;
+        }
+
+        private void PerformStep()
+        {
+            List<Point> toKill = new List<Point>();
+            List<Point> toRevive = new List<Point>();
+
+            for (int x = 0; x < sideBarBeginsAt; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    char current = Map[x, y];
+
+                    Point currentPoint = new Point(x, y);
+
+                    int nAliveNeighbors = GetNumberOfAliveNeighbors(x, y);
+
+                    if (current == Alive)
+                    {
+                        if (nAliveNeighbors < 2) toKill.Add(currentPoint);
+                        else if (nAliveNeighbors > 3) toKill.Add(currentPoint);
+                    }
+                    else
+                    {
+                        if (nAliveNeighbors == 3) toRevive.Add(currentPoint);
+                    }
+                }
+            }
+
+            toKill.ForEach(p => Kill(p.X, p.Y));
+            toRevive.ForEach(p => Revive(p.X, p.Y));
+
+            generation++;
+        }
+
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            CreateStartingMap();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Interval = (int)StepIntervalBox.Value;
+
+            timer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            PerformStep();
+
+            GenerationLabel.Text = generation.ToString();
+        }
+    }
+}
